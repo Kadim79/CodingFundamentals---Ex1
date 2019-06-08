2b0d47e1533828ddb6e546317a846830d5c3e19e
Kadim79 <cdimu@icloud.com>
2019-06-08 12:24:49 +0300

    Exercitiul 1 V1

---
+++ src/com/sdacademy/core/Main.java
@@ -0,0 +1,13 @@
+package com.sdacademy.core;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        int[] array = getArray();
+
+
+
+    }
+}
